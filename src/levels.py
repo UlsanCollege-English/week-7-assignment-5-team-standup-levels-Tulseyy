68b530b9cc1191831f452899ebb9af2d58fbdf66
tulseyy <sashankartulsha@gmail.com>
2025-10-29 14:08:26 +0900

    week 7 assignment 5

--- src/levels.py
+++ src/levels.py
@@ -8,12 +8,59 @@ class Node:
 
 def level_order(root):
     # TODO: return list of lists
-    pass
+    if not root:
+        return []
+    res = []
+    q = deque([root])
+    while q:
+        level = []
+        for _ in range(len(q)):
+            node = q.popleft()
+            level.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        res.append(level)
+    return res
 
 def zigzag_level_order(root):
     # TODO
-    pass
+    if not root:
+        return []
+    res = []
+    q = deque([root])
+    left_to_right = True
+    while q:
+        level = deque()
+        for _ in range(len(q)):
+            node = q.popleft()
+            if left_to_right:
+                level.append(node.val)
+            else:
+                level.appendleft(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        res.append(list(level))
+        left_to_right = not left_to_right
+    return res
 
 def right_side_view(root):
     # TODO
-    pass
+    if not root:
+        return []
+    res = []
+    q = deque([root])
+    while q:
+        last = None
+        for _ in range(len(q)):
+            node = q.popleft()
+            last = node.val
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        res.append(last)
+    return res
